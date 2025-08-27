76eca144c1ee1a745ea53412d6a1de3930ec3cd4
Adam Sharif <adamsharifc@gmail.com>
2025-08-28 00:24:55 +0500

    Refactor manifest configuration to use a function for dynamic permissions and manifest entries based on browser type

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,45 +1,60 @@
-import { defineConfig } from 'wxt';
+import { defineConfig, type UserManifest } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-	extensionApi: "webextension-polyfill",
 	modules: ['@wxt-dev/module-react'],
-	manifest: {
-		permissions: [
-			'declarativeNetRequest',
-			'declarativeNetRequestFeedback',
-			'declarativeNetRequestWithHostAccess',
-			'activeTab',
-			'scripting',
-			'storage'
-		],
-		description: 'A browser extension that blocks ads and trackers.',
-		name: 'Sentinel',
-		host_permissions: [
-			"<all_urls>",
-		],
-		declarative_net_request: {
-			rule_resources: [
-				{
-					id: 'advert-dnr',
-					path: 'advert-dnr.json',
-					enabled: true,
-				},
-				{
-					id: 'tracker-dnr',
-					path: 'tracker-dnr.json',
-					enabled: true,
-				},
-				{
-					id: 'social-dnr',
-					path: 'social-dnr.json',
-					enabled: true,
-				}
+	manifest: ({ browser, manifestVersion }) => {
+		const manifest: UserManifest = {
+			permissions: [
+				'activeTab',
+				'scripting',
+				'storage',
 			],
-		},
-		options_ui: {
-			page: 'options/index.html',
-			open_in_tab: true,
+			host_permissions: ['<all_urls>'],
+			description: 'A browser extension that blocks ads and trackers.',
+			name: 'Sentinel',
+			options_ui: {
+				page: 'options/index.html',
+				open_in_tab: true,
+			},
+			...(browser !== 'firefox'
+				? {
+						permissions: [
+							'activeTab',
+							'scripting',
+							'storage',
+							'declarativeNetRequest',
+							'declarativeNetRequestFeedback',
+							'declarativeNetRequestWithHostAccess',
+						],
+						declarative_net_request: {
+							rule_resources: [
+								{
+									id: 'advert-dnr',
+									path: 'advert-dnr.json',
+									enabled: true,
+								},
+								{
+									id: 'tracker-dnr',
+									path: 'tracker-dnr.json',
+									enabled: true,
+								},
+								{
+									id: 'social-dnr',
+									path: 'social-dnr.json',
+									enabled: true,
+								},
+							],
+						},
+					}
+				: {}),
+		};
+
+		if (browser === 'firefox') {
+			manifest.permissions?.push('webRequest', 'webRequestBlocking');
+			manifest.host_permissions = ['<all_urls>'];
 		}
+
+		return manifest;
 	},
 });
